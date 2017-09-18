12bbebe57745a1a5dc929ef7e1860ec402839056
Bruno Lesieur <bruno.lesieur@gmail.com>
2017-09-18 12:03:40 +0200

    Change condition
    Signed-off-by: Bruno Lesieur <bruno.lesieur@gmail.com>

--- assets/javascripts/boot.js
+++ assets/javascripts/boot.js
@@ -2,7 +2,7 @@
 var websiteLanguage = document.getElementsByTagName('html')[0].getAttribute('lang').substring(0, 2);
 	navigatorLanguage = window.navigator.userLanguage || window.navigator.language;
 
-if (websiteLanguage === 'en' && navigatorLanguage.indexOf('fr') !== -1 && document.referrer.indexOf('://www.orchard-id.fr/')) {
+if (websiteLanguage === 'en' && navigatorLanguage.indexOf('fr') !== -1 && document.referrer.indexOf('://www.orchard-id.fr/') === -1) {
 	location.href = 'https://www.orchard-id.fr/';
 }
 
